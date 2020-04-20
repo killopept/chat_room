0dcc48f5af8b55d3d01b9cc2f048e3d56925add4
zhudh <zhudh@belink.com>
2020-04-20 09:58:24 +0800

    。。

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const wss = new WebSocket.Server({ port: 5501 });
 console.log('server start :5501')
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
-      console.log(data)
+      // console.log(data)
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data);
